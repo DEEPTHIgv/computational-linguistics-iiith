a964048bee3f556cb97b5a4a00513c4f6a5d3f78
DeepthiGV <godabadeepthi@gmail.com>
2020-07-14 20:46:19 +0530

    correct sentences added

---
+++ RDIP/Codes/words.js
@@ -0,0 +1,18 @@
+var set1a= {word1:"John", word2: "ate", word3: "an", word4: "apple", word5: "before", word6: "afternoon"}
+var set1b= {word1:"before", word2: "afternoon", word3: "John", word4: "ate", word5: "an", word6: "apple"}
+var set1c= {word1:"John", word2: "before", word3: "afternoon", word4: "ate", word5: "an", word6: "apple"}
+
+var set2a= {word1:"some", word2: "students", word3: "like", word4: "to", word5: "study", word6: "in", word6: "the", word6: "night"}
+var set2b= {word1: "at", word2: "night",word3:"some", word4: "students", word5: "like", word6: "to", word7: "study"}
+
+var set3a= {word1: "John", word2: "and",word3:"Mary", word4: "went", word5: "to", word6: "church"}
+var set3b= {word1: "Mary", word2: "and",word3:"John", word4: "went", word5: "to", word6: "church"}
+
+var set4a= {word1: "John", word2: "went",word3:"to", word4: "church", word5: "after", word6: "eating"}
+var set4b= {word1: "John",  word2: "after", word3: "eating", word4: "went",word5:"to", word6: "church"}
+var set4c= { word1: "after", word2: "eating", word3: "John", word4: "went",word5:"to", word6: "church"}
+
+
+
+
+
---
+++ RDIP/Codes/words2.js
@@ -0,0 +1,125 @@
+var comb1=["John", "ate", "an", "apple", "before", "afternoon"];
+var comb2=["before", "afternoon", "John", "ate", "an", "apple"];
+var comb3=["John", "before", "afternoon", "ate", "an", "apple"];
+	var set1={comb1, comb2, comb3};
+
+var comb1=["some", "students", "like", "to", "study", "at", "night"];
+var comb2=["at", "night", "some", "students", "like", "to", "study"];
+	var set2={comb1, comb2};
+
+var comb1=["John", "and", "Mary", "went", "to", "church"];
+var comb2=["Mary", "and", "John", "went", "to", "church"];
+	var set3={comb1, comb2};
+
+var comb1=["John", "went", "to", "church","after", "eating" ];
+var comb2=["after", "eating", "John", "went", "to", "church"];
+var comb3=["John","after", "eating", "went", "to", "church"];
+	var set4={comb1, comb2, comb3};
+
+var comb1=["did", "he", "go", "to", "market"];
+var comb2=["he", "did", "go", "to", "market"];
+	var set5={comb1, comb2};
+
+var comb1=["the", "woman", "who", "called", "my", "sister", "sells", "cosmetics"];
+var comb2=["the", "woman", "who", "sells", "cosmetics", "called", "my", "sister"];
+var comb3=["my", "sister", "who", "sells", "cosmetics", "called", "the", "woman"];
+var comb4=["my", "sister", "who", "called", "the", "woman", "sells", "cosmetics"];
+	var set6={comb1, comb2, comb3,comb4};
+
+var comb1=["John", "goes", "to", "the", "library", "and", "studies"];
+var comb2=["John", "studies", "and", "goes", "to", "the", "library"];
+	var set7={comb1, comb2};
+
+var comb1=["John", "ate", "an", "apple", "so", "did", "she"];
+var comb2=["she", "ate", "an", "apple", "so", "did", "John"];
+	var set8={comb1, comb2};
+
+var comb1=["the", "teacher", "returned", "the", "book", "after", "she", "noticed", "the","error"];
+var comb2=["the", "teacher", "noticed", "the","error", "after", "she", "returned", "the", "book"];
+var comb3=["after", "the", "teacher", "returned", "the", "book", "she", "noticed", "the","error"];
+var comb4=["after", "the", "teacher", "noticed", "the","error", "she", "returned", "the", "book"];
+var comb5=["she", "returned", "the", "book", "after", "the", "teacher", "noticed", "the","error"];
+var comb6=["she", "noticed", "the","error", "after", "the", "teacher", "returned", "the", "book"];
+var comb7=["after", "she", "returned", "the", "book", "the", "teacher", "noticed", "the","error"];
+var comb8=["after", "she", "noticed", "the","error", "the", "teacher", "returned", "the", "book"];
+	var set9={comb1, comb2, comb3,comb4,comb5, comb6, comb7,comb8};
+
+var comb1=["I", "told", "her", "that", "I", "brought", "a", "book", "yesterday"];
+var comb2=["I", "told", "her", "yesterday", "that", "I", "brought", "a", "book"];
+var comb3=["yesterday", "I", "told", "her", "that", "I", "brought", "a", "book"];
+var comb4=["I", "brought", "a", "book", "that", "I", "told", "her", "yesterday"];
+var comb5=["I", "brought", "a", "book", "yesterday", "that", "I", "told", "her"];
+var comb6=["yesterday", "I", "brought", "a", "book", "that", "I", "told", "her"];
+	var set10={comb1, comb2, comb3,comb4,comb5, comb6};
+
+	var eng={set1,set2,set3,set4,set5,set6,set7,set8,set9,set10};
+
+var comb1=["राम", "और", "श्याम", "बाजार", "गयें"];
+var comb2=["राम", "और", "श्याम", "गयें", "बाजार"];
+var comb3=["बाजार", "गयें","राम", "और", "श्याम"];
+var comb4=["गयें", "बाजार","राम", "और", "श्याम"];
+	var set1={comb1, comb2, comb3,comb4};
+
+var comb1=["राम", "सोया", "और", "श्याम", "भी"];
+var comb2=[ "श्याम", "सोया", "और","राम", "भी"];
+var comb3=[ "सोया", "श्याम", "और","राम", "भी"];
+var comb4=["सोया", "राम", "और", "श्याम", "भी"];
+	var set2={comb1, comb2, comb3,comb4};
+
+var comb1=["मैंने", "उसे", "बताया", "कि", "राम", "सो", "रहा", "है"];
+var comb2=["मैंने", "उसे", "बताया", "कि", "सो", "रहा", "है", "राम"];
+var comb3=["उसे", "मैंने", "बताया", "कि", "राम", "सो", "रहा", "है"];
+var comb4=["उसे", "मैंने", "बताया", "कि", "सो", "रहा", "है", "राम"];
+var comb5=["मैंने", "बताया", "उसे", "कि", "राम", "सो", "रहा", "है"];
+var comb6=["मैंने", "बताया", "उसे", "कि", "सो", "रहा", "है", "राम"];
+var comb7=["उसे", "बताया", "मैंने", "कि", "राम", "सो", "रहा", "है"];
+var comb8=["उसे", "बताया", "मैंने", "कि", "सो", "रहा", "है", "राम"];
+var comb9=["बताया", "मैंने", "उसे", "कि", "राम", "सो", "रहा", "है"];
+var comb10=["बताया", "मैंने", "उसे", "कि", "सो", "रहा", "है", "राम"];
+var comb11=["बताया", "उसे", "मैंने", "कि", "राम", "सो", "रहा", "है"];
+var comb12=["बताया", "उसे", "मैंने", "कि", "सो", "रहा", "है", "राम"];
+	var set3={comb1, comb2, comb3,comb4,comb5, comb6, comb7,comb8,comb9, comb10, comb11,comb12};
+
+var comb1=["राम", "खाकर","सोया"];
+var comb2=["खाकर","राम", "सोया"];
+var comb3=["राम","सोया", "खाकर"];
+var comb4=["खाकर", "सोया","राम"];
+var comb5=["सोया", "राम","खाकर"];
+var comb6=["सोया","खाकर", "राम"];
+	var set4={comb1, comb2, comb3,comb4,comb5, comb6}
+
+var comb1=["बिल्लियों", "को", "मारकर", "कुत्ता", "सो", "गया"];
+var comb2=["मारकर", "बिल्लियों", "को", "कुत्ता", "सो", "गया"];
+var comb3=["बिल्लियों", "को", "मारकर", "सो", "गया", "कुत्ता"];
+var comb4=["मारकर", "बिल्लियों", "को", "सो", "गया", "कुत्ता"];
+var comb5=["कुत्ता", "सो", "गया", "बिल्लियों", "को", "मारकर"];
+var comb6=["कुत्ता", "सो", "गया", "मारकर", "बिल्लियों", "को"];
+var comb7=["सो", "गया", "कुत्ता", "बिल्लियों", "को", "मारकर"];
+var comb8=["सो", "गया", "कुत्ता", "मारकर", "बिल्लियों", "को"];
+	var set5={comb1, comb2, comb3,comb4,comb5, comb6, comb7,comb8};
+
+var comb1=["एक", "लाल", "किताब", "वहाँ", "है"];
+var comb2=["एक", "लाल", "किताब", "है", "वहाँ"];
+var comb3=["वहाँ", "है", "एक", "लाल", "किताब"];
+var comb4=["है", "वहाँ", "एक", "लाल", "किताब"];
+	var set6={comb1, comb2, comb3,comb4};
+
+var comb1=["एक", "बड़ी", "सी", "किताब", "वहाँ", "है"];
+var comb2=["एक", "बड़ी", "सी", "किताब", "है", "वहाँ"];
+var comb3=["बड़ी", "सी", "एक", "किताब", "वहाँ", "है"];
+var comb4=["बड़ी", "सी", "एक", "किताब", "है", "वहाँ"];
+var comb5=["वहाँ", "है", "एक", "बड़ी", "सी", "किताब"];
+var comb6=["वहाँ", "है", "बड़ी", "सी", "एक", "किताब"];
+var comb7=["है", "वहाँ", "एक", "बड़ी", "सी", "किताब"];
+var comb8=["है", "वहाँ", "बड़ी", "सी", "एक", "किताब"];
+	var set7={comb1, comb2, comb3,comb4,comb5, comb6, comb7,comb8};
+
+		var hin={set1,set2,set3,set4,set5,set6,set7}
+
+document.getElementById("test").innerHTML= hin.set2.comb1;
+
+
+
+
+
+
